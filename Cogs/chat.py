756a6d93194b27a12741d4ebc23c846e04937105
Alex-wood-8277 <helenageorge3@gmail.com>
2021-11-06 11:16:07 +0530

    adding bot ban to chat

--- Cogs/chat.py
+++ Cogs/chat.py
@@ -1,4 +1,7 @@
 #!/bin/python3
+"""
+by using API from personalityforge bot can talk like it does, for documentation visit https://www.personalityforge.com/chatbot-api-docs.php
+"""
 import discord
 import requests
 import database
@@ -10,7 +13,7 @@ from discord.ext import commands
 class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.botID = 106996  # NSFW Annabelle Lee = 106996, Laurel Sweet = 71367 SFW Cyber Ty = 63906, prob = 23958
+        self.botID = 106996  # NSFW Annabelle Lee = 106996, Laurel Sweet = 71367 Lil Neko = 148149 SFW Cyber Ty = 63906, prob = 23958
         self.key = environ['FORGE']
         self.base_chat_url = f"https://www.personalityforge.com/api/chat/?apiKey={self.key}&chatBotID={self.botID}&message="  # "{message}&externalID=<externalID>&firstName=<firstName>&lastName=<lastName>&gender=<gender>"
 
@@ -18,44 +21,48 @@ class Chat(commands.Cog):
     async def on_message(self, message):
         if message.author.bot or not message.content.startswith('..'):
             return
-        if set(database.get_config('chat', message.guild.id)) & set([role.id for role in message.author.roles]) or database.get_config('chat', message.guild.id) == [0]:
-            ctx = await self.bot.get_context(message)
-            async with ctx.typing():
-                if not message.channel.nsfw:
-                    await ctx.reply(f'This channel is not NSFW, I only chat in NSFW channels to be safe.')
-                    return
+        ban_data = database.is_botban(message.author.id)
+        if ban_data is None:
+            if set(database.get_config('chat', message.guild.id)) & set([role.id for role in message.author.roles]) or database.get_config('chat', message.guild.id) == [0]:
+                ctx = await self.bot.get_context(message)
+                async with ctx.typing():
+                    if not message.channel.nsfw:
+                        await ctx.reply(f'This channel is not NSFW, I only chat in NSFW channels to be safe.')
+                        return
 
-                if set(database.get_config('domme', message.guild.id)) & set([role.id for role in message.author.roles]):
-                    gender = 'f'
-                else:
-                    gender = 'm'
-                message = message.content[2:]
-                url = f"{self.base_chat_url}{message}&externalID={ctx.author.id}&gender={gender}"
-                data = requests.get(url).json()
-                if data['success'] == 1:
-                    message = data['message']['message'].replace('<br>', '\n')
-                    emotion = data['message']['emotion']
-                    if emotion == 'happy-9':
-                        # await ctx.message.add_reaction(emoji='simp:858985009905664040')
-                        message = message + ' <:giggle:897777342791942225>'
-                    if 'cowboy!' in message:
-                        NSFW_reply = ['*ignoring*', 'Go watch porn pervert.', 'I am not a Pervert like you', 'I am not a pathetic slut like you.', ]
-                        message = choice(NSFW_reply)
-
-                    # print(message)
-                    # print(emotion)
-                    await ctx.reply(message)
-                else:
-                    alex_wood = self.bot.get_user(855057142297264139)
-                    await alex_wood.send(f"`{data}`")
-                    await ctx.reply(f' I can\'t talk righ now {ctx.author.mention} I have personal stuff to do.')
+                    if set(database.get_config('domme', message.guild.id)) & set([role.id for role in message.author.roles]):
+                        gender = 'f'
+                    else:
+                        gender = 'm'
+                    message = message.content[2:]
+                    url = f"{self.base_chat_url}{message}&externalID={ctx.author.id}&gender={gender}"
+                    data = requests.get(url).json()
+                    if data['success'] == 1:
+                        message = data['message']['message'].replace('<br>', '\n')
+                        emotion = data['message']['emotion']
+                        if emotion == 'happy-9':
+                            # await ctx.message.add_reaction(emoji='simp:858985009905664040')
+                            message = message + ' <:giggle:897777342791942225>'
+                        if 'cowboy!' in message:
+                            NSFW_reply = ['*ignoring*', 'Go watch porn pervert.', 'I am not a Pervert like you', 'I am not a pathetic slut like you.', ]
+                            message = choice(NSFW_reply)
+                        await ctx.reply(message)
+                    else:
+                        alex_wood = self.bot.get_user(855057142297264139)
+                        await alex_wood.send(f"`{data}`")
+                        await ctx.reply(f' I can\'t talk righ now {ctx.author.mention} I have personal stuff to do.')
+            else:
+                roles = '>'
+                for r in database.get_config('chat', message.guild.id):
+                    roles = f"{roles} <@&{r}>\n>"
+                embed = discord.Embed(description=f"you don't have any of the following roles to talk to me.\n{roles[:-2]}", color=0xF2A2C0)
+                await message.channel.send(embed=embed)
+                return
         else:
-            roles = '>'
-            for r in database.get_config('chat', message.guild.id):
-                roles = f"{roles} <@&{r}>\n>"
-            embed = discord.Embed(description=f"you don't have any of the following roles to talk to me.\n{roles[:-2]}", color=0xF2A2C0)
-            await message.channel.send(embed=embed)
-            return
+            embed = discord.Embed(title='Bot Ban',
+                                  description=f"{message.author.mention} you are banned from using {self.bot.user.mention} till {ban_data[1]}",
+                                  color=0xF2A2C0)
+            await message.reply(embed=embed)
 
 
 def setup(bot):
