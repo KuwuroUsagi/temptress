5a0bda0fd223ce48faf49b3c30db41bdf27c5698
Alex-wood-8277 <helenageorge3@gmail.com>
2021-11-10 00:11:08 +0530

    rps init work in progress

--- Cogs/gambling.py
+++ Cogs/gambling.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 import discord
+from discord_components import *
 import database
 import asyncio
 import random
@@ -14,6 +15,7 @@ cur = con.cursor()
 class Gambling(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        DiscordComponents(bot)
 
     ##############################################################################
     #                                                                            #
@@ -188,11 +190,100 @@ class Gambling(commands.Cog):
             await ctx.send(embed=embed)
             
             
-    @commands.command(aliases=['sr'])
+    @commands.command(aliases=['rps'])
     @commands.guild_only()
-    @commands.cooldown(5, 1 * 60 * 60, commands.BucketType.user)
-    async def subrace(self, ctx, domme: discord.Member, bet: int):
-        pass
+    # @commands.cooldown(5, 1 * 60 * 60, commands.BucketType.user)
+    async def rockpaper(self, ctx, member: discord.Member, bet: int):
+        if ctx.author.bot:
+            return
+        return
+        ban_data = database.is_botban(ctx.author.id)
+        if ban_data is not None:
+            embed = discord.Embed(title='Bot ban',
+                                  description=f"{ctx.author.mention} you are banned from using {self.bot.user.mention} till <t:{ban_data[1]}:F>",
+                                  color=0xF2A2C0)
+            await ctx.send(embed=embed)
+            return
+        elif database.is_botban(member.id) is not None:
+            embed = discord.Embed(title='Bot ban',
+                                  description=f"{member.mention} is banned from using {self.bot.user.mention}.",
+                                  color=0xF2A2C0)
+            await ctx.send(embed=embed)
+            return
+
+        author_coin = database.get_money(ctx.author.id, ctx.guild.id)[2]
+        member_coin = database.get_money(member.id, ctx.guild.id)[2]
+        
+        if bet < 10:
+            embed = discord.Embed(description=f"<:staff:897777248839540757> You need to bet at least 10 <a:pinkcoin:900000697288892416>",
+                                  color=0xF2A2C0)
+            await ctx.send(embed=embed)
+        elif bet > author_coin:
+            embed = discord.Embed(description=f"{ctx.author.mention} really?, <:staff:897777248839540757> You only have {author_coin} <a:pinkcoin:900000697288892416>",
+                                  color=0xF2A2C0)
+            await ctx.send(embed=embed)
+        elif bet > member_coin:
+            embed = discord.Embed(description=f"<:staff:897777248839540757>, {member.mention} only have {author_coin} <a:pinkcoin:900000697288892416>",
+                                  color=0xF2A2C0)
+            await ctx.send(embed=embed)
+        else:
+            m = await ctx.send(f"{ctx.author.nick or ctx.author.name} is thinking...\n{member.nick or member.name} is thinking...",
+                               components=[[Button(style=ButtonStyle.blue, label="Rock", emoji='🗿'),
+                                            Button(style=ButtonStyle.blue, label='Paper', emoji='🧻'),
+                                            Button(style=ButtonStyle.blue, label='Scissors', emoji='✂️',)]])
+            try:
+                def check(res):
+                    return res.user == ctx.author or res.user == member and res.message.id == m.id
+                response = await self.bot.wait_for('button_click', timeout=30, check=check)
+                await response.respond(type=6)
+                if response.user == ctx.author:
+                    author_choice = response.component.label
+                    await m.edit(f"{ctx.author.nick or ctx.author.name} made a choice.\n{member.nick or member.name} is thinking...")
+                    try:
+                        def check(res):
+                            return res.user == member and res.message.id == m.id
+                        response = await self.bot.wait_for('button_click', timeout=30, check=check)
+                        await response.respond(type=6)
+                        member_choice = response.component.label
+                    except asyncio.TimeoutError:
+                        await m.edit(f"I guess {member.nick or member.name} is not interested to play with {ctx.author.nick or ctx.author.name}",
+                                     components=[])
+                else:
+                    member_choice = response.component.label
+                    await m.edit(f"{ctx.author.nick or ctx.author.name} is thinking...\n{member.nick or member.name} made a choice.")
+                    try:
+                        def check(res):
+                            return res.user == ctx.author and res.message.id == m.id
+                        response = await self.bot.wait_for('button_click', timeout=30, check=check)
+                        await response.respond(type=6)
+                        author_choice = response.component.label
+                    except asyncio.TimeoutError:
+                        await m.edit(f"I guess {ctx.author.nick or ctx.author.name} is not interested to play with {member.nick or member.name}",
+                                     components=[])
+                        
+                        
+                        
+            except asyncio.TimeoutError:
+                await m.edit(f"I guess {ctx.author.nick or ctx.author.name} and {member.nick or member.name} don't wanna play!!",
+                             components=[])
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     ##############################################################################
     #                                                                            #
