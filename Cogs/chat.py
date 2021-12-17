e73aa2e58c9118e5ddef6a995e2c6c1a2cd332ea
Kuro <jeluu2@gmail.com>
2021-12-17 05:44:31 -0800

    troubleshooting

--- Cogs/chat.py
+++ Cogs/chat.py
@@ -13,7 +13,7 @@ from discord.ext import commands
 class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.botID = 157181  # NSFW Annabelle Lee = 106996, Laurel Sweet = 71367 Lil Neko = 148149 SFW Cyber Ty = 63906, prob = 23958
+        self.botID = 106996 # NSFW Annabelle Lee = 106996, Laurel Sweet = 71367 Lil Neko = 148149 SFW Cyber Ty = 63906, prob = 23958, 157181
         self.key = environ['FORGE']
         self.base_chat_url = f"https://www.personalityforge.com/api/chat/?apiKey={self.key}&chatBotID={self.botID}&message="  # "{message}&externalID=<externalID>&firstName=<firstName>&lastName=<lastName>&gender=<gender>"
 
@@ -21,37 +21,46 @@ class Chat(commands.Cog):
     async def on_message(self, message):
         if message.author.bot or not message.content.startswith('..'):
             return
+
+        print('first part is fine')
         ban_data = database.is_botban(message.author.id)
+        print('second part is fine')
         if ban_data is None:
             if set(database.get_config('chat', message.guild.id)) & set([role.id for role in message.author.roles]) or database.get_config('chat', message.guild.id) == [0]:
+                print('third part is fine')
                 ctx = await self.bot.get_context(message)
+
                 async with ctx.typing():
                     if not message.channel.nsfw:
                         await ctx.reply(f'This channel is not NSFW, I only chat in NSFW channels to be safe.')
                         return
+                    
+                    print('(after typing)')
 
                     if set(database.get_config('domme', message.guild.id)) & set([role.id for role in message.author.roles]):
                         gender = 'f'
                     else:
                         gender = 'm'
+
+                    print('fourth part is fine')
                     message = message.content[2:]
                     url = f"{self.base_chat_url}{message}&externalID={ctx.author.id}&gender={gender}"
                     data = requests.get(url).json()
-                    if data['success'] == 1:
-                        message = data['message']['message'].replace('<br>', '\n')
-                        emotion = data['message']['emotion']
-                        if emotion == 'happy-9':
-                            # await ctx.message.add_reaction(emoji='simp:858985009905664040')
-                            message = message + ' <:blushcat:879560974925959178>'
-                        if 'cowboy!' in message:
-                            NSFW_reply = ['*ignoring*', 'Go watch porn pervert.', 'I am not a Pervert like you', 'I am not a pathetic slut like you.', ]
-                            message = choice(NSFW_reply)
-                        await ctx.reply(message)
-                    else:
-                        alex_wood = self.bot.get_user(855057142297264139)
-                        await alex_wood.send(f"`{data}`")
-                        await ctx.reply(f' I can\'t talk right now {ctx.author.mention} I have personal stuff to do.')
+                    print(data)
+                    message_data = data['message']['message'].replace('<br>', '\n')
+                    emotion = data['message']['emotion']
+                    if emotion == 'happy-9':
+                        # await ctx.message.add_reaction(emoji='simp:858985009905664040')
+                        message_data = message_data + ' <:giggle:897777342791942225>'
+                    print('fifth part is fine')
+                    if 'cowboy!' in message_data:
+                        NSFW_reply = ['*ignoring*', 'Go watch porn pervert.', 'I am not a Pervert like you', 'I am not a pathetic slut like you.', ]
+                        message_data = choice(NSFW_reply)
+                    print('last part is also fine')
+                    await ctx.reply(message_data)
+                        
             else:
+                print('role error')
                 roles = '>'
                 for r in database.get_config('chat', message.guild.id):
                     roles = f"{roles} <@&{r}>\n>"
