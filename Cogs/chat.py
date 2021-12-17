85c5617bbe970c333e62be45425c7b5aaff9a6ff
Kuro <jeluu2@gmail.com>
2021-12-17 06:04:45 -0800

    environment variable for bot id

--- Cogs/chat.py
+++ Cogs/chat.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+r#!/bin/python3
 """
 by using API from personalityforge bot can talk like it does, for documentation visit https://www.personalityforge.com/chatbot-api-docs.php
 """
@@ -13,7 +13,7 @@ from discord.ext import commands
 class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.botID = 157181 # NSFW Annabelle Lee = 106996, Laurel Sweet = 71367 Lil Neko = 148149 SFW Cyber Ty = 63906, prob = 23958, 157181
+        self.botID = environ['BOT'] # NSFW Annabelle Lee = 106996, Laurel Sweet = 71367 Lil Neko = 148149 SFW Cyber Ty = 63906, prob = 23958, 
         self.key = environ['FORGE']
         self.base_chat_url = f"https://www.personalityforge.com/api/chat/?apiKey={self.key}&chatBotID={self.botID}&message="  # "{message}&externalID=<externalID>&firstName=<firstName>&lastName=<lastName>&gender=<gender>"
 
